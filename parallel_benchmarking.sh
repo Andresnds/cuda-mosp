02c78633d6fda80932ff23e51d3daf8266625486
Andre Saraiva <andresnds@hotmail.com>
2016-11-15 23:29:13 -0200

    Added benchmarking scripts

---
+++ parallel_benchmarking.sh
@@ -0,0 +1,141 @@
+#!/bin/sh
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbo_10_10.txt > output/Harvey/wbo_10_10_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbo_10_10.txt > output/Harvey/wbo_10_10_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbo_10_20.txt > output/Harvey/wbo_10_20_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbo_10_20.txt > output/Harvey/wbo_10_20_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbo_10_30.txt > output/Harvey/wbo_10_30_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbo_10_30.txt > output/Harvey/wbo_10_30_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbo_15_15.txt > output/Harvey/wbo_15_15_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbo_15_15.txt > output/Harvey/wbo_15_15_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbo_15_30.txt > output/Harvey/wbo_15_30_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbo_15_30.txt > output/Harvey/wbo_15_30_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbo_20_10.txt > output/Harvey/wbo_20_10_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbo_20_10.txt > output/Harvey/wbo_20_10_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbo_20_20.txt > output/Harvey/wbo_20_20_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbo_20_20.txt > output/Harvey/wbo_20_20_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbo_30_10.txt > output/Harvey/wbo_30_10_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbo_30_10.txt > output/Harvey/wbo_30_10_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbo_30_15.txt > output/Harvey/wbo_30_15_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbo_30_15.txt > output/Harvey/wbo_30_15_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbo_30_30.txt > output/Harvey/wbo_30_30_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbo_30_30.txt > output/Harvey/wbo_30_30_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbop_10_10.txt > output/Harvey/wbop_10_10_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbop_10_10.txt > output/Harvey/wbop_10_10_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbop_10_20.txt > output/Harvey/wbop_10_20_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbop_10_20.txt > output/Harvey/wbop_10_20_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbop_10_30.txt > output/Harvey/wbop_10_30_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbop_10_30.txt > output/Harvey/wbop_10_30_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbop_15_15.txt > output/Harvey/wbop_15_15_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbop_15_15.txt > output/Harvey/wbop_15_15_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbop_15_30.txt > output/Harvey/wbop_15_30_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbop_15_30.txt > output/Harvey/wbop_15_30_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbop_20_10.txt > output/Harvey/wbop_20_10_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbop_20_10.txt > output/Harvey/wbop_20_10_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbop_20_20.txt > output/Harvey/wbop_20_20_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbop_20_20.txt > output/Harvey/wbop_20_20_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbop_30_10.txt > output/Harvey/wbop_30_10_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbop_30_10.txt > output/Harvey/wbop_30_10_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbop_30_15.txt > output/Harvey/wbop_30_15_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbop_30_15.txt > output/Harvey/wbop_30_15_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbop_30_30.txt > output/Harvey/wbop_30_30_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbop_30_30.txt > output/Harvey/wbop_30_30_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbp_10_10.txt > output/Harvey/wbp_10_10_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbp_10_10.txt > output/Harvey/wbp_10_10_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbp_10_20.txt > output/Harvey/wbp_10_20_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbp_10_20.txt > output/Harvey/wbp_10_20_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbp_10_30.txt > output/Harvey/wbp_10_30_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbp_10_30.txt > output/Harvey/wbp_10_30_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbp_15_15.txt > output/Harvey/wbp_15_15_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbp_15_15.txt > output/Harvey/wbp_15_15_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbp_15_30.txt > output/Harvey/wbp_15_30_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbp_15_30.txt > output/Harvey/wbp_15_30_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbp_20_10.txt > output/Harvey/wbp_20_10_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbp_20_10.txt > output/Harvey/wbp_20_10_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbp_20_20.txt > output/Harvey/wbp_20_20_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbp_20_20.txt > output/Harvey/wbp_20_20_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbp_30_10.txt > output/Harvey/wbp_30_10_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbp_30_10.txt > output/Harvey/wbp_30_10_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbp_30_15.txt > output/Harvey/wbp_30_15_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbp_30_15.txt > output/Harvey/wbp_30_15_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Harvey/wbp_30_30.txt > output/Harvey/wbp_30_30_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Harvey/wbp_30_30.txt > output/Harvey/wbp_30_30_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Miller/Miller19.txt > output/Miller/Miller19_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Miller/Miller19.txt > output/Miller/Miller19_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Shaw/ShawInstances.txt > output/Shaw/ShawInstances_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Shaw/ShawInstances.txt > output/Shaw/ShawInstances_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Simonis/problem_10_10.dat > output/Simonis/problem_10_10_parallel_dp.dat
+timeout 10m ./parallel bf < ChallengeInstances2005/Simonis/problem_10_10.dat > output/Simonis/problem_10_10_parallel_bf.dat
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Simonis/problem_10_20.dat > output/Simonis/problem_10_20_parallel_dp.dat
+timeout 10m ./parallel bf < ChallengeInstances2005/Simonis/problem_10_20.dat > output/Simonis/problem_10_20_parallel_bf.dat
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Simonis/problem_15_15.dat > output/Simonis/problem_15_15_parallel_dp.dat
+timeout 10m ./parallel bf < ChallengeInstances2005/Simonis/problem_15_15.dat > output/Simonis/problem_15_15_parallel_bf.dat
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Simonis/problem_15_30.dat > output/Simonis/problem_15_30_parallel_dp.dat
+timeout 10m ./parallel bf < ChallengeInstances2005/Simonis/problem_15_30.dat > output/Simonis/problem_15_30_parallel_bf.dat
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Simonis/problem_20_10.dat > output/Simonis/problem_20_10_parallel_dp.dat
+timeout 10m ./parallel bf < ChallengeInstances2005/Simonis/problem_20_10.dat > output/Simonis/problem_20_10_parallel_bf.dat
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Simonis/problem_20_20.dat > output/Simonis/problem_20_20_parallel_dp.dat
+timeout 10m ./parallel bf < ChallengeInstances2005/Simonis/problem_20_20.dat > output/Simonis/problem_20_20_parallel_bf.dat
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Simonis/problem_30_10.dat > output/Simonis/problem_30_10_parallel_dp.dat
+timeout 10m ./parallel bf < ChallengeInstances2005/Simonis/problem_30_10.dat > output/Simonis/problem_30_10_parallel_bf.dat
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Simonis/problem_30_15.dat > output/Simonis/problem_30_15_parallel_dp.dat
+timeout 10m ./parallel bf < ChallengeInstances2005/Simonis/problem_30_15.dat > output/Simonis/problem_30_15_parallel_bf.dat
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Simonis/problem_30_30.dat > output/Simonis/problem_30_30_parallel_dp.dat
+timeout 10m ./parallel bf < ChallengeInstances2005/Simonis/problem_30_30.dat > output/Simonis/problem_30_30_parallel_bf.dat
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Simonis/problem_40_20.dat > output/Simonis/problem_40_20_parallel_dp.dat
+timeout 10m ./parallel bf < ChallengeInstances2005/Simonis/problem_40_20.dat > output/Simonis/problem_40_20_parallel_bf.dat
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Wilson/gp100by100.txt > output/Wilson/gp100by100_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Wilson/gp100by100.txt > output/Wilson/gp100by100_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Wilson/gp50by50.txt > output/Wilson/gp50by50_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Wilson/gp50by50.txt > output/Wilson/gp50by50_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Wilson/nrwsLarger4.txt > output/Wilson/nrwsLarger4_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Wilson/nrwsLarger4.txt > output/Wilson/nrwsLarger4_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Wilson/nwrsSmaller4.txt > output/Wilson/nwrsSmaller4_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Wilson/nwrsSmaller4.txt > output/Wilson/nwrsSmaller4_parallel_bf.txt
+
+timeout 10m ./parallel dp < ChallengeInstances2005/Wilson/sp4.txt > output/Wilson/sp4_parallel_dp.txt
+timeout 10m ./parallel bf < ChallengeInstances2005/Wilson/sp4.txt > output/Wilson/sp4_parallel_bf.txt
---
+++ sequential_benchmarking.sh
@@ -0,0 +1,141 @@
+#!/bin/sh
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbo_10_10.txt > output/Harvey/wbo_10_10_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbo_10_10.txt > output/Harvey/wbo_10_10_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbo_10_20.txt > output/Harvey/wbo_10_20_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbo_10_20.txt > output/Harvey/wbo_10_20_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbo_10_30.txt > output/Harvey/wbo_10_30_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbo_10_30.txt > output/Harvey/wbo_10_30_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbo_15_15.txt > output/Harvey/wbo_15_15_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbo_15_15.txt > output/Harvey/wbo_15_15_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbo_15_30.txt > output/Harvey/wbo_15_30_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbo_15_30.txt > output/Harvey/wbo_15_30_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbo_20_10.txt > output/Harvey/wbo_20_10_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbo_20_10.txt > output/Harvey/wbo_20_10_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbo_20_20.txt > output/Harvey/wbo_20_20_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbo_20_20.txt > output/Harvey/wbo_20_20_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbo_30_10.txt > output/Harvey/wbo_30_10_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbo_30_10.txt > output/Harvey/wbo_30_10_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbo_30_15.txt > output/Harvey/wbo_30_15_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbo_30_15.txt > output/Harvey/wbo_30_15_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbo_30_30.txt > output/Harvey/wbo_30_30_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbo_30_30.txt > output/Harvey/wbo_30_30_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbop_10_10.txt > output/Harvey/wbop_10_10_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbop_10_10.txt > output/Harvey/wbop_10_10_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbop_10_20.txt > output/Harvey/wbop_10_20_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbop_10_20.txt > output/Harvey/wbop_10_20_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbop_10_30.txt > output/Harvey/wbop_10_30_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbop_10_30.txt > output/Harvey/wbop_10_30_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbop_15_15.txt > output/Harvey/wbop_15_15_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbop_15_15.txt > output/Harvey/wbop_15_15_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbop_15_30.txt > output/Harvey/wbop_15_30_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbop_15_30.txt > output/Harvey/wbop_15_30_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbop_20_10.txt > output/Harvey/wbop_20_10_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbop_20_10.txt > output/Harvey/wbop_20_10_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbop_20_20.txt > output/Harvey/wbop_20_20_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbop_20_20.txt > output/Harvey/wbop_20_20_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbop_30_10.txt > output/Harvey/wbop_30_10_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbop_30_10.txt > output/Harvey/wbop_30_10_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbop_30_15.txt > output/Harvey/wbop_30_15_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbop_30_15.txt > output/Harvey/wbop_30_15_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbop_30_30.txt > output/Harvey/wbop_30_30_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbop_30_30.txt > output/Harvey/wbop_30_30_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbp_10_10.txt > output/Harvey/wbp_10_10_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbp_10_10.txt > output/Harvey/wbp_10_10_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbp_10_20.txt > output/Harvey/wbp_10_20_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbp_10_20.txt > output/Harvey/wbp_10_20_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbp_10_30.txt > output/Harvey/wbp_10_30_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbp_10_30.txt > output/Harvey/wbp_10_30_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbp_15_15.txt > output/Harvey/wbp_15_15_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbp_15_15.txt > output/Harvey/wbp_15_15_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbp_15_30.txt > output/Harvey/wbp_15_30_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbp_15_30.txt > output/Harvey/wbp_15_30_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbp_20_10.txt > output/Harvey/wbp_20_10_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbp_20_10.txt > output/Harvey/wbp_20_10_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbp_20_20.txt > output/Harvey/wbp_20_20_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbp_20_20.txt > output/Harvey/wbp_20_20_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbp_30_10.txt > output/Harvey/wbp_30_10_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbp_30_10.txt > output/Harvey/wbp_30_10_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbp_30_15.txt > output/Harvey/wbp_30_15_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbp_30_15.txt > output/Harvey/wbp_30_15_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Harvey/wbp_30_30.txt > output/Harvey/wbp_30_30_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Harvey/wbp_30_30.txt > output/Harvey/wbp_30_30_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Miller/Miller19.txt > output/Miller/Miller19_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Miller/Miller19.txt > output/Miller/Miller19_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Shaw/ShawInstances.txt > output/Shaw/ShawInstances_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Shaw/ShawInstances.txt > output/Shaw/ShawInstances_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Simonis/problem_10_10.dat > output/Simonis/problem_10_10_sequential_dp.dat
+timeout 10m ./sequential bf < ChallengeInstances2005/Simonis/problem_10_10.dat > output/Simonis/problem_10_10_sequential_bf.dat
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Simonis/problem_10_20.dat > output/Simonis/problem_10_20_sequential_dp.dat
+timeout 10m ./sequential bf < ChallengeInstances2005/Simonis/problem_10_20.dat > output/Simonis/problem_10_20_sequential_bf.dat
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Simonis/problem_15_15.dat > output/Simonis/problem_15_15_sequential_dp.dat
+timeout 10m ./sequential bf < ChallengeInstances2005/Simonis/problem_15_15.dat > output/Simonis/problem_15_15_sequential_bf.dat
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Simonis/problem_15_30.dat > output/Simonis/problem_15_30_sequential_dp.dat
+timeout 10m ./sequential bf < ChallengeInstances2005/Simonis/problem_15_30.dat > output/Simonis/problem_15_30_sequential_bf.dat
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Simonis/problem_20_10.dat > output/Simonis/problem_20_10_sequential_dp.dat
+timeout 10m ./sequential bf < ChallengeInstances2005/Simonis/problem_20_10.dat > output/Simonis/problem_20_10_sequential_bf.dat
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Simonis/problem_20_20.dat > output/Simonis/problem_20_20_sequential_dp.dat
+timeout 10m ./sequential bf < ChallengeInstances2005/Simonis/problem_20_20.dat > output/Simonis/problem_20_20_sequential_bf.dat
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Simonis/problem_30_10.dat > output/Simonis/problem_30_10_sequential_dp.dat
+timeout 10m ./sequential bf < ChallengeInstances2005/Simonis/problem_30_10.dat > output/Simonis/problem_30_10_sequential_bf.dat
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Simonis/problem_30_15.dat > output/Simonis/problem_30_15_sequential_dp.dat
+timeout 10m ./sequential bf < ChallengeInstances2005/Simonis/problem_30_15.dat > output/Simonis/problem_30_15_sequential_bf.dat
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Simonis/problem_30_30.dat > output/Simonis/problem_30_30_sequential_dp.dat
+timeout 10m ./sequential bf < ChallengeInstances2005/Simonis/problem_30_30.dat > output/Simonis/problem_30_30_sequential_bf.dat
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Simonis/problem_40_20.dat > output/Simonis/problem_40_20_sequential_dp.dat
+timeout 10m ./sequential bf < ChallengeInstances2005/Simonis/problem_40_20.dat > output/Simonis/problem_40_20_sequential_bf.dat
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Wilson/gp100by100.txt > output/Wilson/gp100by100_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Wilson/gp100by100.txt > output/Wilson/gp100by100_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Wilson/gp50by50.txt > output/Wilson/gp50by50_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Wilson/gp50by50.txt > output/Wilson/gp50by50_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Wilson/nrwsLarger4.txt > output/Wilson/nrwsLarger4_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Wilson/nrwsLarger4.txt > output/Wilson/nrwsLarger4_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Wilson/nwrsSmaller4.txt > output/Wilson/nwrsSmaller4_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Wilson/nwrsSmaller4.txt > output/Wilson/nwrsSmaller4_sequential_bf.txt
+
+timeout 10m ./sequential dp < ChallengeInstances2005/Wilson/sp4.txt > output/Wilson/sp4_sequential_dp.txt
+timeout 10m ./sequential bf < ChallengeInstances2005/Wilson/sp4.txt > output/Wilson/sp4_sequential_bf.txt
