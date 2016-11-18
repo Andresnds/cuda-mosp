cb4e7f202516cdfbb1877a0d08d72a20f1464848
Andre Saraiva <andresnds@hotmail.com>
2016-11-18 07:18:48 -0200

    Fix echo message

--- parallel_benchmarking.sh
+++ parallel_benchmarking.sh
@@ -1,86 +1,86 @@
 #!/bin/sh
 timeout 30m ./parallel dp < andre_10_2.txt > output/andre_10_2_parallel_dp.txt
-echo "Done parallel dp Harvey/wbo_10_10.txt"
+echo "Done parallel dp andre_10_2.txtt"
 timeout 30m ./parallel bf < andre_10_2.txt > output/andre_10_2_parallel_bf.txt
-echo "Done parallel bf Harvey/wbo_10_10.txt"
+echo "Done parallel bf andre_10_2.txtt"
 
 timeout 30m ./parallel dp < andre_10_3.txt > output/andre_10_3_parallel_dp.txt
-echo "Done parallel dp Harvey/wbo_10_10.txt"
+echo "Done parallel dp andre_10_3.txtt"
 timeout 30m ./parallel bf < andre_10_3.txt > output/andre_10_3_parallel_bf.txt
-echo "Done parallel bf Harvey/wbo_10_10.txt"
+echo "Done parallel bf andre_10_3.txtt"
 
 timeout 30m ./parallel dp < andre_10_4.txt > output/andre_10_4_parallel_dp.txt
-echo "Done parallel dp Harvey/wbo_10_10.txt"
+echo "Done parallel dp andre_10_4.txtt"
 timeout 30m ./parallel bf < andre_10_4.txt > output/andre_10_4_parallel_bf.txt
-echo "Done parallel bf Harvey/wbo_10_10.txt"
+echo "Done parallel bf andre_10_4.txtt"
 
 timeout 30m ./parallel dp < andre_10_5.txt > output/andre_10_5_parallel_dp.txt
-echo "Done parallel dp Harvey/wbo_10_10.txt"
+echo "Done parallel dp andre_10_5.txtt"
 timeout 30m ./parallel bf < andre_10_5.txt > output/andre_10_5_parallel_bf.txt
-echo "Done parallel bf Harvey/wbo_10_10.txt"
+echo "Done parallel bf andre_10_5.txtt"
 
 timeout 30m ./parallel dp < andre_10_6.txt > output/andre_10_6_parallel_dp.txt
-echo "Done parallel dp Harvey/wbo_10_10.txt"
+echo "Done parallel dp andre_10_6.txtt"
 timeout 30m ./parallel bf < andre_10_6.txt > output/andre_10_6_parallel_bf.txt
-echo "Done parallel bf Harvey/wbo_10_10.txt"
+echo "Done parallel bf andre_10_6.txtt"
 
 timeout 30m ./parallel dp < andre_10_7.txt > output/andre_10_7_parallel_dp.txt
-echo "Done parallel dp Harvey/wbo_10_10.txt"
+echo "Done parallel dp andre_10_7.txtt"
 timeout 30m ./parallel bf < andre_10_7.txt > output/andre_10_7_parallel_bf.txt
-echo "Done parallel bf Harvey/wbo_10_10.txt"
+echo "Done parallel bf andre_10_7.txtt"
 
 timeout 30m ./parallel dp < andre_10_8.txt > output/andre_10_8_parallel_dp.txt
-echo "Done parallel dp Harvey/wbo_10_10.txt"
+echo "Done parallel dp andre_10_8.txtt"
 timeout 30m ./parallel bf < andre_10_8.txt > output/andre_10_8_parallel_bf.txt
-echo "Done parallel bf Harvey/wbo_10_10.txt"
+echo "Done parallel bf andre_10_8.txtt"
 
 timeout 30m ./parallel dp < andre_10_9.txt > output/andre_10_9_parallel_dp.txt
-echo "Done parallel dp Harvey/wbo_10_10.txt"
+echo "Done parallel dp andre_10_9.txtt"
 timeout 30m ./parallel bf < andre_10_9.txt > output/andre_10_9_parallel_bf.txt
-echo "Done parallel bf Harvey/wbo_10_10.txt"
+echo "Done parallel bf andre_10_9.txtt"
 
 timeout 30m ./parallel dp < andre_10_10.txt > output/andre_10_10_parallel_dp.txt
-echo "Done parallel dp Harvey/wbo_10_10.txt"
+echo "Done parallel dp andre_10_10.txt"
 timeout 30m ./parallel bf < andre_10_10.txt > output/andre_10_10_parallel_bf.txt
-echo "Done parallel bf Harvey/wbo_10_10.txt"
+echo "Done parallel bf andre_10_10.txt"
 
 timeout 30m ./parallel dp < andre_10_11.txt > output/andre_10_11_parallel_dp.txt
-echo "Done parallel dp Harvey/wbo_10_10.txt"
+echo "Done parallel dp andre_10_11.txt"
 timeout 30m ./parallel bf < andre_10_11.txt > output/andre_10_11_parallel_bf.txt
-echo "Done parallel bf Harvey/wbo_10_10.txt"
+echo "Done parallel bf andre_10_11.txt"
 
 timeout 30m ./parallel dp < andre_10_12.txt > output/andre_10_12_parallel_dp.txt
-echo "Done parallel dp Harvey/wbo_10_10.txt"
+echo "Done parallel dp andre_10_12.txt"
 timeout 30m ./parallel bf < andre_10_12.txt > output/andre_10_12_parallel_bf.txt
-echo "Done parallel bf Harvey/wbo_10_10.txt"
+echo "Done parallel bf andre_10_12.txt"
 
 timeout 30m ./parallel dp < andre_10_13.txt > output/andre_10_13_parallel_dp.txt
-echo "Done parallel dp Harvey/wbo_10_10.txt"
+echo "Done parallel dp andre_10_13.txt"
 timeout 30m ./parallel bf < andre_10_13.txt > output/andre_10_13_parallel_bf.txt
-echo "Done parallel bf Harvey/wbo_10_10.txt"
+echo "Done parallel bf andre_10_13.txt"
 
 timeout 30m ./parallel dp < andre_10_14.txt > output/andre_10_14_parallel_dp.txt
-echo "Done parallel dp Harvey/wbo_10_10.txt"
+echo "Done parallel dp andre_10_14.txt"
 timeout 30m ./parallel bf < andre_10_14.txt > output/andre_10_14_parallel_bf.txt
-echo "Done parallel bf Harvey/wbo_10_10.txt"
+echo "Done parallel bf andre_10_14.txt"
 
 timeout 30m ./parallel dp < andre_10_15.txt > output/andre_10_15_parallel_dp.txt
-echo "Done parallel dp Harvey/wbo_10_10.txt"
+echo "Done parallel dp andre_10_15.txt"
 timeout 30m ./parallel bf < andre_10_15.txt > output/andre_10_15_parallel_bf.txt
-echo "Done parallel bf Harvey/wbo_10_10.txt"
+echo "Done parallel bf andre_10_15.txt"
 
 timeout 30m ./parallel dp < andre_10_16.txt > output/andre_10_16_parallel_dp.txt
-echo "Done parallel dp Harvey/wbo_10_10.txt"
+echo "Done parallel dp andre_10_16.txt"
 timeout 30m ./parallel bf < andre_10_16.txt > output/andre_10_16_parallel_bf.txt
-echo "Done parallel bf Harvey/wbo_10_10.txt"
+echo "Done parallel bf andre_10_16.txt"
 
 timeout 30m ./parallel dp < andre_10_17.txt > output/andre_10_17_parallel_dp.txt
-echo "Done parallel dp Harvey/wbo_10_10.txt"
+echo "Done parallel dp andre_10_17.txt"
 timeout 30m ./parallel bf < andre_10_17.txt > output/andre_10_17_parallel_bf.txt
-echo "Done parallel bf Harvey/wbo_10_10.txt"
+echo "Done parallel bf andre_10_17.txt"
 
 timeout 30m ./parallel dp < andre_10_18.txt > output/andre_10_18_parallel_dp.txt
-echo "Done parallel dp Harvey/wbo_10_10.txt"
+echo "Done parallel dp andre_10_18.txt"
 timeout 30m ./parallel bf < andre_10_18.txt > output/andre_10_18_parallel_bf.txt
-echo "Done parallel bf Harvey/wbo_10_10.txt"
+echo "Done parallel bf andre_10_18.txt"
 
