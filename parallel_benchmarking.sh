1a23b2b87bc040a1c59a5fabfde62f57d243eee3
Andre Saraiva <andresnds@hotmail.com>
2016-11-20 06:11:43 -0200

    Fix from last commit

--- parallel_benchmarking.sh
+++ parallel_benchmarking.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
-timeout 30m ./parallel dp < andre_10_2.txt > output/andre_10_2_parallel_dp.txt
-echo "Done parallel dp andre_10_2.txt"
+timeout 30m ./parallel bf < andre_10_2.txt > output/andre_10_2_parallel_bf.txt
+echo "Done parallel bf andre_10_2.txt"
 
-timeout 30m ./parallel dp < andre_10_3.txt > output/andre_10_3_parallel_dp.txt
-echo "Done parallel dp andre_10_3.txt"
+timeout 30m ./parallel bf < andre_10_3.txt > output/andre_10_3_parallel_bf.txt
+echo "Done parallel bf andre_10_3.txt"
 
-timeout 30m ./parallel dp < andre_10_4.txt > output/andre_10_4_parallel_dp.txt
-echo "Done parallel dp andre_10_4.txt"
+timeout 30m ./parallel bf < andre_10_4.txt > output/andre_10_4_parallel_bf.txt
+echo "Done parallel bf andre_10_4.txt"
 
-timeout 30m ./parallel dp < andre_10_5.txt > output/andre_10_5_parallel_dp.txt
-echo "Done parallel dp andre_10_5.txt"
+timeout 30m ./parallel bf < andre_10_5.txt > output/andre_10_5_parallel_bf.txt
+echo "Done parallel bf andre_10_5.txt"
 
-timeout 30m ./parallel dp < andre_10_6.txt > output/andre_10_6_parallel_dp.txt
-echo "Done parallel dp andre_10_6.txt"
+timeout 30m ./parallel bf < andre_10_6.txt > output/andre_10_6_parallel_bf.txt
+echo "Done parallel bf andre_10_6.txt"
 
-timeout 30m ./parallel dp < andre_10_7.txt > output/andre_10_7_parallel_dp.txt
-echo "Done parallel dp andre_10_7.txt"
+timeout 30m ./parallel bf < andre_10_7.txt > output/andre_10_7_parallel_bf.txt
+echo "Done parallel bf andre_10_7.txt"
 
-timeout 30m ./parallel dp < andre_10_8.txt > output/andre_10_8_parallel_dp.txt
-echo "Done parallel dp andre_10_8.txt"
+timeout 30m ./parallel bf < andre_10_8.txt > output/andre_10_8_parallel_bf.txt
+echo "Done parallel bf andre_10_8.txt"
 
-timeout 30m ./parallel dp < andre_10_9.txt > output/andre_10_9_parallel_dp.txt
-echo "Done parallel dp andre_10_9.txt"
+timeout 30m ./parallel bf < andre_10_9.txt > output/andre_10_9_parallel_bf.txt
+echo "Done parallel bf andre_10_9.txt"
