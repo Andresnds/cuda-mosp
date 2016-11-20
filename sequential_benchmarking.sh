a0c2fe700f23b941a469d8013a3e2df1abbfcca7
Andre Saraiva <andresnds@hotmail.com>
2016-11-19 23:42:57 -0200

    andre experiment output

--- sequential_benchmarking.sh
+++ sequential_benchmarking.sh
@@ -1,86 +1,16 @@
 #!/bin/sh
-timeout 30m ./sequential dp < andre_10_2.txt > output/andre_10_2_sequential_dp.txt
-echo "Done sequential dp andre_10_2.txt"
-timeout 30m ./sequential bf < andre_10_2.txt > output/andre_10_2_sequential_bf.txt
-echo "Done sequential bf andre_10_2.txt"
-
-timeout 30m ./sequential dp < andre_10_3.txt > output/andre_10_3_sequential_dp.txt
-echo "Done sequential dp andre_10_3.txt"
-timeout 30m ./sequential bf < andre_10_3.txt > output/andre_10_3_sequential_bf.txt
-echo "Done sequential bf andre_10_3.txt"
-
-timeout 30m ./sequential dp < andre_10_4.txt > output/andre_10_4_sequential_dp.txt
-echo "Done sequential dp andre_10_4.txt"
-timeout 30m ./sequential bf < andre_10_4.txt > output/andre_10_4_sequential_bf.txt
-echo "Done sequential bf andre_10_4.txt"
-
-timeout 30m ./sequential dp < andre_10_5.txt > output/andre_10_5_sequential_dp.txt
-echo "Done sequential dp andre_10_5.txt"
-timeout 30m ./sequential bf < andre_10_5.txt > output/andre_10_5_sequential_bf.txt
-echo "Done sequential bf andre_10_5.txt"
-
-timeout 30m ./sequential dp < andre_10_6.txt > output/andre_10_6_sequential_dp.txt
-echo "Done sequential dp andre_10_6.txt"
-timeout 30m ./sequential bf < andre_10_6.txt > output/andre_10_6_sequential_bf.txt
-echo "Done sequential bf andre_10_6.txt"
-
-timeout 30m ./sequential dp < andre_10_7.txt > output/andre_10_7_sequential_dp.txt
-echo "Done sequential dp andre_10_7.txt"
-timeout 30m ./sequential bf < andre_10_7.txt > output/andre_10_7_sequential_bf.txt
-echo "Done sequential bf andre_10_7.txt"
-
-timeout 30m ./sequential dp < andre_10_8.txt > output/andre_10_8_sequential_dp.txt
-echo "Done sequential dp andre_10_8.txt"
-timeout 30m ./sequential bf < andre_10_8.txt > output/andre_10_8_sequential_bf.txt
-echo "Done sequential bf andre_10_8.txt"
-
-timeout 30m ./sequential dp < andre_10_9.txt > output/andre_10_9_sequential_dp.txt
-echo "Done sequential dp andre_10_9.txt"
-timeout 30m ./sequential bf < andre_10_9.txt > output/andre_10_9_sequential_bf.txt
-echo "Done sequential bf andre_10_9.txt"
-
-timeout 30m ./sequential dp < andre_10_10.txt > output/andre_10_10_sequential_dp.txt
-echo "Done sequential dp andre_10_10.txt"
-timeout 30m ./sequential bf < andre_10_10.txt > output/andre_10_10_sequential_bf.txt
-echo "Done sequential bf andre_10_10.txt"
-
-timeout 30m ./sequential dp < andre_10_11.txt > output/andre_10_11_sequential_dp.txt
-echo "Done sequential dp andre_10_11.txt"
-timeout 30m ./sequential bf < andre_10_11.txt > output/andre_10_11_sequential_bf.txt
-echo "Done sequential bf andre_10_11.txt"
-
-timeout 30m ./sequential dp < andre_10_12.txt > output/andre_10_12_sequential_dp.txt
-echo "Done sequential dp andre_10_12.txt"
-timeout 30m ./sequential bf < andre_10_12.txt > output/andre_10_12_sequential_bf.txt
-echo "Done sequential bf andre_10_12.txt"
-
-timeout 30m ./sequential dp < andre_10_13.txt > output/andre_10_13_sequential_dp.txt
-echo "Done sequential dp andre_10_13.txt"
-timeout 30m ./sequential bf < andre_10_13.txt > output/andre_10_13_sequential_bf.txt
-echo "Done sequential bf andre_10_13.txt"
-
 timeout 30m ./sequential dp < andre_10_14.txt > output/andre_10_14_sequential_dp.txt
 echo "Done sequential dp andre_10_14.txt"
-timeout 30m ./sequential bf < andre_10_14.txt > output/andre_10_14_sequential_bf.txt
-echo "Done sequential bf andre_10_14.txt"
 
 timeout 30m ./sequential dp < andre_10_15.txt > output/andre_10_15_sequential_dp.txt
 echo "Done sequential dp andre_10_15.txt"
-timeout 30m ./sequential bf < andre_10_15.txt > output/andre_10_15_sequential_bf.txt
-echo "Done sequential bf andre_10_15.txt"
 
 timeout 30m ./sequential dp < andre_10_16.txt > output/andre_10_16_sequential_dp.txt
 echo "Done sequential dp andre_10_16.txt"
-timeout 30m ./sequential bf < andre_10_16.txt > output/andre_10_16_sequential_bf.txt
-echo "Done sequential bf andre_10_16.txt"
 
 timeout 30m ./sequential dp < andre_10_17.txt > output/andre_10_17_sequential_dp.txt
 echo "Done sequential dp andre_10_17.txt"
-timeout 30m ./sequential bf < andre_10_17.txt > output/andre_10_17_sequential_bf.txt
-echo "Done sequential bf andre_10_17.txt"
 
 timeout 30m ./sequential dp < andre_10_18.txt > output/andre_10_18_sequential_dp.txt
 echo "Done sequential dp andre_10_18.txt"
-timeout 30m ./sequential bf < andre_10_18.txt > output/andre_10_18_sequential_bf.txt
-echo "Done sequential bf andre_10_18.txt"
 
