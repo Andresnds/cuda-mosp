4a9d86a5aae99ebc2f3f0913ceb53a3b642a5905
Andre Saraiva <andresnds@hotmail.com>
2016-11-20 06:07:53 -0200

    Change bench

--- parallel_benchmarking.sh
+++ parallel_benchmarking.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
-timeout 30m ./parallel dp < andre_10_14.txt > output/andre_10_14_parallel_dp.txt
-echo "Done parallel dp andre_10_14.txt"
+timeout 30m ./parallel dp < andre_10_2.txt > output/andre_10_2_parallel_dp.txt
+echo "Done parallel dp andre_10_2.txt"
 
-timeout 30m ./parallel dp < andre_10_15.txt > output/andre_10_15_parallel_dp.txt
-echo "Done parallel dp andre_10_15.txt"
+timeout 30m ./parallel dp < andre_10_3.txt > output/andre_10_3_parallel_dp.txt
+echo "Done parallel dp andre_10_3.txt"
 
-timeout 30m ./parallel dp < andre_10_16.txt > output/andre_10_16_parallel_dp.txt
-echo "Done parallel dp andre_10_16.txt"
+timeout 30m ./parallel dp < andre_10_4.txt > output/andre_10_4_parallel_dp.txt
+echo "Done parallel dp andre_10_4.txt"
 
-timeout 30m ./parallel dp < andre_10_17.txt > output/andre_10_17_parallel_dp.txt
-echo "Done parallel dp andre_10_17.txt"
+timeout 30m ./parallel dp < andre_10_5.txt > output/andre_10_5_parallel_dp.txt
+echo "Done parallel dp andre_10_5.txt"
 
-timeout 30m ./parallel dp < andre_10_18.txt > output/andre_10_18_parallel_dp.txt
-echo "Done parallel dp andre_10_18.txt"
+timeout 30m ./parallel dp < andre_10_6.txt > output/andre_10_6_parallel_dp.txt
+echo "Done parallel dp andre_10_6.txt"
 
+timeout 30m ./parallel dp < andre_10_7.txt > output/andre_10_7_parallel_dp.txt
+echo "Done parallel dp andre_10_7.txt"
+
+timeout 30m ./parallel dp < andre_10_8.txt > output/andre_10_8_parallel_dp.txt
+echo "Done parallel dp andre_10_8.txt"
+
+timeout 30m ./parallel dp < andre_10_9.txt > output/andre_10_9_parallel_dp.txt
+echo "Done parallel dp andre_10_9.txt"
